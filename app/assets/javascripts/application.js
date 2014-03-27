69e2ea8b9e643146362d400501da92fc2ef3c85e
Nícolas Iensen <nicolas@engage.is>
2014-03-27 16:42:42 -0300

    [#68352822] add slide 2 and parallax

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,4 +16,24 @@
 //= require turbolinks
 //= require_tree .
 
-$(function(){ $(document).foundation(); });
+$(function(){
+  $(document).foundation();
+
+  $(window).scroll(function(){
+    // Parallax
+    scrollRatio = window.scrollY/$(document).height();
+    scrollProgress = ((window.scrollY/$(document).height()) * 100) + "%";
+    invertedScrollProgress = (100 - ((window.scrollY/$(document).height()) * 100)) + "%";
+    scrollProgressPx = ((window.scrollY/$(document).height()) * 1000) + "px";
+    invertedScrollProgressPx = (10000 - (window.scrollY/$(document).height()) * 10000) + "px";
+
+    slide1Height = $(".slide-1").height() + parseInt($(".slide-1").css("padding-top")) + parseInt($(".slide-1").css("padding-bottom"));
+    slide1BgHeight = 896;
+    slide1ScrollRatio = window.scrollY/($(".slide-1").position().top + slide1Height);
+    slide1Offset = slide1BgHeight - slide1Height;
+    slide1Position = (slide1Offset * slide1ScrollRatio) + "px";
+
+    $(".slide-1").css("background-position", "center " + slide1Position);
+    $(".slide-2").css("background-position", scrollProgress + " 40%");
+  });
+});
