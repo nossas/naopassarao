7ee509ac2c9b66e321de939c951c2374f7099133
Nícolas Iensen <nicolas@engage.is>
2014-03-28 13:42:29 -0300

    [#68352784] add Poke#add_to_mailchimp_segment

--- app/models/poke.rb
+++ app/models/poke.rb
@@ -1,2 +1,16 @@
 class Poke < ActiveRecord::Base
+  belongs_to :user
+
+  after_create { self.delay.add_to_mailchimp_segment }
+
+  def add_to_mailchimp_segment
+    if !Rails.env.test?
+      begin
+        Gibbon::API.lists.subscribe(id: ENV["MAILCHIMP_LIST_ID"], email: {email: self.user.email}, merge_vars: {FNAME: self.user.first_name, LNAME: self.user.last_name, CITY: self.user.city}, double_optin: false, update_existing: true)
+        Gibbon::API.lists.static_segment_members_add(id: ENV["MAILCHIMP_LIST_ID"], seg_id: ENV["MAILCHIMP_SEGMENT_ID"], batch: [{email: self.user.email}])
+      rescue Exception => e
+        Rails.logger.error e
+      end
+    end
+  end
 end
