e0a068be66aa6a6ea02d49339a9846403a253fdc
Nícolas Iensen <nicolas@iensen.me>
2014-03-31 11:08:46 -0300

    [#68352826] add Call model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140328214426) do
+ActiveRecord::Schema.define(version: 20140331135946) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "calls", force: true do |t|
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "delayed_jobs", force: true do |t|
     t.integer  "priority",   default: 0, null: false
