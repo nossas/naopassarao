d25ac29da33e4585d546c54cdd72b296d5acad8e
Nícolas Iensen <nicolas@iensen.me>
2014-04-01 15:09:05 -0300

    Change targets

--- config/initializers/targets.rb
+++ config/initializers/targets.rb
@@ -1,5 +1,5 @@
 if Rails.env.production?
-  TARGETS_EMAILS = ['servic@metro.sp.gov.br', 'ouvidoria@metrosp.com.br']
+  TARGETS_EMAILS = ['imprensa@metrosp.com.br', 'ouvidoria@metrosp.com.br']
 else
   raise "TEST_EMAIL is missing" if ENV["TEST_EMAIL"].nil?
   TARGETS_EMAILS = ENV["TEST_EMAIL"]
